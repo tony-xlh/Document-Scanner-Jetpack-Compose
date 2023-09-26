e0dadb911b313e805e1f06b75ebe99137376c745
xulihang <xulihanghai@163.com>
2023-09-26 15:24:31 +0800

    remove preview

--- app/src/main/java/com/tonyxlh/documentscanner/MainActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/MainActivity.kt
@@ -64,19 +64,3 @@ fun AddButton() {
     }
 
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DocumentScannerTheme {
-        Greeting("Android")
-    }
-}
