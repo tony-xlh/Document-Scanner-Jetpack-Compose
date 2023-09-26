9b9d946c96c23d102580461912ef5f26dfc1cd63
xulihang <xulihanghai@163.com>
2023-09-26 17:59:37 +0800

    move scanners and service to content scope

--- app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,17 +14,25 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tonyxlh.docscan4j.DynamsoftService
+import com.tonyxlh.docscan4j.Scanner
 import com.tonyxlh.documentscanner.ui.theme.DocumentScannerTheme
 
 class ScannerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var scanners = mutableListOf<Scanner>()
+            val service = DynamsoftService(
+                "http://192.168.8.65:18622",
+                "t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI="
+            )
             LaunchedEffect(key1 = true){
                 try {
-                    val service = DynamsoftService("http://192.168.8.65:18622","t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI=")
-                    val scanners = service.getScanners();
-                    Log.d("DM",scanners.size.toString())
+                    Log.d("DM","request start")
+                    Thread {
+                        scanners = service.getScanners();
+                        Log.d("DM", scanners.size.toString())
+                    }.start()
                 }catch (e:Exception){
                     Log.d("DM",e.stackTraceToString())
                 }
@@ -35,6 +44,11 @@ class ScannerActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    Button(onClick = {
+                        Log.d("DM","Clicked: "+scanners.size);
+                    }){
+                        Text("Click")
+                    }
                 }
             }
         }
