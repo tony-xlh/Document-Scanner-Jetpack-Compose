ecc402c5dfd3304d7435806a6da608f2bc379f6e
xulihang <xulihanghai@163.com>
2023-09-26 17:23:05 +0800

    add docscan4j

--- app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
@@ -1,6 +1,7 @@
 package com.tonyxlh.documentscanner
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,14 +9,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.tonyxlh.docscan4j.DynamsoftService
 import com.tonyxlh.documentscanner.ui.theme.DocumentScannerTheme
 
 class ScannerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LaunchedEffect(key1 = true){
+                try {
+                    val service = DynamsoftService("http://192.168.8.65:18622","t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI=")
+                    val scanners = service.getScanners();
+                    Log.d("DM",scanners.size.toString())
+                }catch (e:Exception){
+                    Log.d("DM",e.stackTraceToString())
+                }
+            }
             DocumentScannerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
