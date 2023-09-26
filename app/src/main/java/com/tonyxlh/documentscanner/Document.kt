4c0883ef6b69fa6d729276835d643d87c4096378
xulihang <xulihanghai@163.com>
2023-09-26 15:59:28 +0800

    add DocumentList to list documents

---
+++ app/src/main/java/com/tonyxlh/documentscanner/Document.kt
@@ -0,0 +1,4 @@
+package com.tonyxlh.documentscanner
+
+
+class Document(val date: Long, val name: String)
--- app/src/main/java/com/tonyxlh/documentscanner/MainActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -20,6 +29,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tonyxlh.documentscanner.ui.theme.DocumentScannerTheme
@@ -31,36 +42,62 @@ class MainActivity : ComponentActivity() {
             DocumentScannerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize().
+                                        padding(10.dp),
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        AddButton()
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                        ) {
+                            sampleDocuments().forEach { doc ->
+                                DocumentItem(doc)
+                            }
+                        }
+                        FloatingActionButton(
+                            modifier = Modifier
+                                .padding(all = 16.dp)
+                                .align(alignment = Alignment.BottomEnd),
+                            onClick = {
+                                Log.d("DBR","clicked");
+                            }
+                        ) {
+                            Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+                        }
                     }
                 }
             }
         }
     }
-
 }
 
 @Composable
-fun AddButton() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        FloatingActionButton(
+fun DocumentItem(doc: Document) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.thumbnail),
+            contentDescription = null,
             modifier = Modifier
-                .padding(all = 16.dp)
-                .align(alignment = Alignment.BottomEnd),
-            onClick = {
-                Log.d("DBR","clicked");
-            }
-        ) {
-            Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = doc.name,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = doc.date.toString())
         }
     }
+}
 
+fun sampleDocuments(): MutableList<Document> {
+    val docs = mutableListOf<Document>();
+    docs.add(Document(1695714698605,"Document 1"));
+    docs.add(Document(1695714799615,"Document 2"));
+    return docs
 }
