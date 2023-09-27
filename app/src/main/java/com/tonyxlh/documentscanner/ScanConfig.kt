f1f179e715641391d42a084f7c682667afc21f13
xulihang <xulihanghai@163.com>
2023-09-27 15:27:49 +0800

    add a scan button

--- app/src/main/java/com/tonyxlh/documentscanner/ScanConfig.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScanConfig.kt
@@ -1,11 +1,12 @@
 package com.tonyxlh.documentscanner
 
 import com.tonyxlh.docscan4j.Capabilities
+import com.tonyxlh.docscan4j.CapabilitySetup
 import com.tonyxlh.docscan4j.DeviceConfiguration
 import com.tonyxlh.docscan4j.Scanner
 
 class ScanConfig (
     var scanner: Scanner,
     var deviceConfig: DeviceConfiguration,
-    var caps: Capabilities
+    var pixelType: CapabilitySetup
 )
--- app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
@@ -1,24 +1,19 @@
 package com.tonyxlh.documentscanner
 
-import android.graphics.Paint.Cap
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -30,41 +25,52 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.tonyxlh.docscan4j.Capabilities
+import com.tonyxlh.docscan4j.CapabilitySetup
 import com.tonyxlh.docscan4j.DeviceConfiguration
 import com.tonyxlh.docscan4j.DynamsoftService
 import com.tonyxlh.docscan4j.Scanner
 import com.tonyxlh.documentscanner.ui.theme.DocumentScannerTheme
 
+
 class ScannerActivity : ComponentActivity() {
     var scanConfig:ScanConfig? = null
+    val service = DynamsoftService(
+        "http://192.168.8.65:18622",
+        "t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI="
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var images by mutableStateOf(emptyList<Bitmap>())
+        var openDialog = mutableStateOf(false)
+        var scanners by mutableStateOf(emptyList<Scanner>())
         setContent {
-            var scanners = remember {mutableListOf<Scanner>()};
-            val service = DynamsoftService(
-                "http://192.168.8.65:18622",
-                "t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI="
-            )
             LaunchedEffect(key1 = true){
                     Log.d("DM","request start")
                     Thread {
                         try {
-                            scanners = service.getScanners();
+                            var newScanners = mutableStateListOf<Scanner>()
+                            service.getScanners().forEach {
+                                newScanners.add(it)
+                            }
+                            scanners = newScanners
                             if (scanners.size>0) {
+                                var pixelType = CapabilitySetup()
+                                pixelType.capability=257
+                                pixelType.curValue=0
+                                pixelType.exception="ignore"
                                 scanConfig = ScanConfig(scanners.get(0),
                                     DeviceConfiguration(),
-                                    Capabilities()
+                                    pixelType
                                 )
                             }
                             Log.d("DM", scanners.size.toString())
@@ -74,7 +80,7 @@ class ScannerActivity : ComponentActivity() {
                     }.start()
             }
             DocumentScannerTheme {
-                var openDialog = remember { mutableStateOf(false) }
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -84,6 +90,16 @@ class ScannerActivity : ComponentActivity() {
                         Button(
                             onClick = {
                                 Log.d("DM",scanConfig.toString())
+                                val scanned = scan()
+                                var newImages = mutableStateListOf<Bitmap>()
+                                scanned.forEach {
+                                    newImages.add(it)
+                                }
+                                images.forEach {
+                                    newImages.add(it)
+                                }
+                                images = newImages
+
                             }
                         ){
                             Text("Scan")
@@ -105,26 +121,50 @@ class ScannerActivity : ComponentActivity() {
                                 },scanConfig,scanners)
                             }
                         }
+                        images.forEach { image ->
+                            Image(
+                                bitmap = image.asImageBitmap(),
+                                contentDescription = ""
+                            )
+                        }
                     }
                 }
             }
         }
     }
+
+    fun scan(): MutableList<Bitmap> {
+        var images = mutableListOf<Bitmap>();
+        val t = Thread {
+            var caps = Capabilities()
+            caps.capabilities.add(scanConfig!!.pixelType)
+            val jobID = service.createScanJob(scanConfig!!.scanner,scanConfig!!.deviceConfig,caps)
+            var image = service.nextDocument(jobID)
+            while (image != null) {
+                val bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
+                images.add(bitmap)
+                image = service.nextDocument(jobID)
+            }
+        }
+        t.start()
+        t.join()
+        return images
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScannerSettingsDialog(onDismissRequest: (scanConfig:ScanConfig?) -> Unit,currentScanConfig:ScanConfig?, scanners:MutableList<Scanner>) {
+fun ScannerSettingsDialog(onDismissRequest: (scanConfig:ScanConfig?) -> Unit,currentScanConfig:ScanConfig?, scanners:List<Scanner>) {
     var expanded by remember { mutableStateOf(false) }
     var selectedScannerName by remember { mutableStateOf("") }
     var selectedScanner:Scanner? = null
     var deviceConfig:DeviceConfiguration = DeviceConfiguration()
-    var caps:Capabilities = Capabilities()
+    var pixelType:CapabilitySetup = CapabilitySetup()
     if (currentScanConfig != null){
         selectedScanner = currentScanConfig.scanner
         selectedScannerName = selectedScanner.name
         deviceConfig = currentScanConfig.deviceConfig
-        caps = currentScanConfig.caps
+        pixelType = currentScanConfig.pixelType
     }
     Dialog(onDismissRequest = { onDismissRequest(null) }) {
         Card(
@@ -173,7 +213,7 @@ fun ScannerSettingsDialog(onDismissRequest: (scanConfig:ScanConfig?) -> Unit,cur
                 }
                 Button(onClick = {
                     if (selectedScanner != null) {
-                        val scanConfig = ScanConfig(selectedScanner!!,deviceConfig,caps)
+                        val scanConfig = ScanConfig(selectedScanner!!,deviceConfig,pixelType)
                         onDismissRequest(scanConfig)
                     }else{
                         onDismissRequest(null)
