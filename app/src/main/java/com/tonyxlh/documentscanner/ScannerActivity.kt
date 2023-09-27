002979a4e1734906d8decc03e432df3972c97484
xulihang <xulihanghai@163.com>
2023-09-27 17:44:16 +0800

    add a spacer

--- app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -39,6 +40,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -98,7 +100,7 @@ class ScannerActivity : ComponentActivity() {
                 ) {
                     Column {
                         Row (
-                            modifier = Modifier.padding(5.dp)
+                            modifier = Modifier.padding(5.dp),
                         ) {
                             Button(
                                 onClick = {
@@ -124,6 +126,12 @@ class ScannerActivity : ComponentActivity() {
                                 }
                             ){
                                 Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                            }
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Button(
+                                onClick = {
+                            }) {
+                                Text("Save")
                             }
                             when {
                                 openDialog.value -> {
