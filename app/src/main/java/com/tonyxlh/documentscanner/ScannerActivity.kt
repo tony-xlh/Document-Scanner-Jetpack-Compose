856ffe12d947e17e2f6329807bdb09847820d327
xulihang <xulihanghai@163.com>
2023-09-27 10:37:07 +0800

    add a settings dialog

--- app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
+++ app/src/main/java/com/tonyxlh/documentscanner/ScannerActivity.kt
@@ -2,17 +2,37 @@ package com.tonyxlh.documentscanner
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.tonyxlh.docscan4j.DynamsoftService
 import com.tonyxlh.docscan4j.Scanner
 import com.tonyxlh.documentscanner.ui.theme.DocumentScannerTheme
@@ -21,33 +41,44 @@ class ScannerActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var scanners = mutableListOf<Scanner>()
+            var scanners = remember {mutableListOf<Scanner>()};
             val service = DynamsoftService(
                 "http://192.168.8.65:18622",
                 "t0068MgAAAEm8KzOlKD/AG56RuTf2RSTo4ajLgVpDBfQkmIJYY7yrDj3jbzQpRfQRzGnACr7S1F/7Da6REO20jmF3QR4VDXI="
             )
             LaunchedEffect(key1 = true){
-                try {
                     Log.d("DM","request start")
                     Thread {
-                        scanners = service.getScanners();
-                        Log.d("DM", scanners.size.toString())
+                        try {
+                            scanners = service.getScanners();
+                            Log.d("DM", scanners.size.toString())
+                        }catch (e:Exception){
+                            Log.d("DM",e.stackTraceToString())
+                        }
                     }.start()
-                }catch (e:Exception){
-                    Log.d("DM",e.stackTraceToString())
-                }
             }
             DocumentScannerTheme {
+                var openDialog = remember { mutableStateOf(false) }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-                    Button(onClick = {
-                        Log.d("DM","Clicked: "+scanners.size);
-                    }){
-                        Text("Click")
+                    Column {
+                        Button(
+                            onClick = {
+                                openDialog.value = true;
+                            }
+                        ){
+                            Text("Settings")
+                        }
+                        when {
+                            openDialog.value -> {
+                                ScannerSettingsDialog({
+                                    openDialog.value = false
+                                },scanners)
+                            }
+                        }
                     }
                 }
             }
@@ -56,17 +87,48 @@ class ScannerActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ScannerSettingsDialog(onDismissRequest: () -> Unit,scanners:MutableList<Scanner>) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedScanner by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DocumentScannerTheme {
-        Greeting("Android")
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column {
+                Row {
+                    Text(
+                        text = "Scanners:"
+                    )
+                    Button(onClick = {
+                        expanded = true
+                    }){
+                        Text(
+                            text = selectedScanner,
+                        )
+                    }
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                    ) {
+                        scanners.forEach { doc ->
+                            DropdownMenuItem(
+                                text = { Text(doc.name) },
+                                onClick = { Toast.makeText(context, doc.name, Toast.LENGTH_SHORT).show() }
+                            )
+                        }
+                    }
+                }
+
+            }
+
+
+        }
     }
 }
